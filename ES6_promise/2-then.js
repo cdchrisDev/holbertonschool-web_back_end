e8732eda9a7c0b015fb117a99a2143d05eb29031
cdchrisDev <juan.feli.lopez@gmail.com>
2024-07-28 18:17:59 -0500

    Lint error FIX

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   const keep = promise.then(() => {
     console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
+    return {
+      status: 200,
+      body: 'success',
     };
-  }).catch(() => new Error());     
-  return keep
+  }).catch(() => new Error());
+  return keep;
 }
